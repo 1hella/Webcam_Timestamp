037b9177e2bd3fd191355781d616c2e2490d2f1d
Stephen Wanhella <stephen.wanhella@gmail.com>
2023-03-11 23:14:55 -0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+import cv2
+import streamlit as st
+import time
+
+st.title('Motion Detector')
+start = st.button('start camera')
+
+if start:
+    streamlit_image = st.image([])
+    camera = cv2.VideoCapture(0)
+
+    while True:
+        check, frame = camera.read()
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        day = time.strftime("%A")
+        formatted_time = time.strftime("%H:%M:%S")
+
+        cv2.putText(img=frame, text=day, org=(50, 50),
+                    fontFace=cv2.FONT_HERSHEY_PLAIN, fontScale=2, color=(255, 255, 255),
+                    thickness=2, lineType=cv2.LINE_AA)
+        cv2.putText(img=frame, text=formatted_time, org=(50, 80),
+                    fontFace=cv2.FONT_HERSHEY_PLAIN, fontScale=2, color=(255, 0, 0),
+                    thickness=2, lineType=cv2.LINE_AA)
+
+        streamlit_image.image(frame)
---
+++ readme.md
@@ -0,0 +1,3 @@
+# What is this project?
+
+It's an opencv streamlit app to show a webcam and put a timestamp overtop of it.
